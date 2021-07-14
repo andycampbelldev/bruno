88cf0acbd4b809e23f24ef2bf6be131edfba6815
Andrew Campbell <AndyC@andrews-mbp.home>
2021-07-13 21:13:17 -0700

    update Beer index page to use the d-none class to show/hide a beer's recipes and brews depending on the button that has been clicked, and ensure only one of these sections is displayed at a time

--- public/js/beer-index.js
+++ public/js/beer-index.js
@@ -1,5 +1,27 @@
 // remove table scroll-cue when a user starts scrolling
-let recipes = document.querySelectorAll('.beer-index-recipes-table-container');
-for (let recipe of recipes) {
-  recipe.addEventListener('scroll', toggleScrollCue)  
+let recipeLists = document.querySelectorAll('.beer-index-recipes-table-container');
+for (let r of recipeLists) {
+  r.addEventListener('scroll', toggleScrollCue)  
+}
+
+
+
+// event listener on Recipes and Brews buttons - when one is clicked, remove the 'show' class from the other to cause it to collapse
+// let buttons = document.querySelectorAll('.beer-index-buttons button')
+// for (let button of buttons) {
+//     button.addEventListener('click', function() {
+//         let target = this.getAttribute('data-target');
+//         target = target.indexOf('recipes') > 0 ? target.replace('recipes', 'brews') : target.replace('brews', 'recipes');
+//         document.querySelector(target).classList.remove('show');
+//     })
+// }
+
+let buttons = document.querySelectorAll('.beer-index-buttons button')
+for (let button of buttons) {
+    button.addEventListener('click', function() {
+        let showTarget = this.getAttribute('data-target');
+        let hideTarget = showTarget.indexOf('recipes') > 0 ? showTarget.replace('recipes', 'brews') : showTarget.replace('brews', 'recipes');
+        document.querySelector(showTarget).classList.remove('d-none');
+        document.querySelector(hideTarget).classList.add('d-none');
+    })
 }
